b84535d486626dfb547ec5b309e2e2358421fd6a
Brian Geertsma <briange@tuta.io>
2023-02-03 00:13:15 -0500

    updaing base install and adding baisc components, tailwind, daisyui

---
+++ components/header.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Header() {
+  return (
+    <Head>
+      <link rel="shortcut icon" href="/swimming-pool.png" />
+      <title>SK Clean</title>
+      <meta
+        name="description"
+        content="The best clean for your pool."
+      />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+  );
+}
