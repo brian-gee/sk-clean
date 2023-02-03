6bb0201bf5e2aaa5dd4c90c5e6a430a9b850748d
Brian Geertsma <briange@tuta.io>
2023-02-03 17:09:01 -0500

    updating DaisyUI theme and adding footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,15 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '@/components/header';
 import Nav from '@/components/nav';
+import Footer from '@/components/footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main>
+    <main data-theme="business">
       <Header />
       <Nav />
       <Component {...pageProps} />
+      <Footer />
     </main>
   );
 }
