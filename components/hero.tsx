6fc06a2a3d62d99db1e5177f35f1995f4116ab94
Brian Geertsma <briange@tuta.io>
2023-02-03 01:05:15 -0500

    updating components - nav/hero

---
+++ components/hero.tsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <main>
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage: '/swimming-pool.png' }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl">Your Pool</h1>
+            <h3 className="mb-5 text-3xl">Reimagined</h3>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,9 @@
+import Hero from '@/components/hero';
+
 export default function Home() {
   return (
     <main>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Your Pool</h1>
-            <h2 className="text-3xl py-2">
-              Reimagined
-            </h2>
-            <button className="btn btn-primary mt-6">Get Started</button>
-          </div>
-        </div>
-      </div>
+      <Hero />
     </main>
   );
 }
