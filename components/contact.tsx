01bb277d9ac8863366277d63dda765866e18a43b
Brian Geertsma <briange@tuta.io>
2023-02-05 16:51:42 -0500

    adding contact section

---
+++ components/contact.tsx
@@ -0,0 +1,18 @@
+export default function Contact({}) {
+  return (
+    <main className="flex justify-center">
+      <div className="card w-96">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title uppercase">Talk to us</h2>
+          <p>
+            Have any questions? We are always open to talk about your business,
+            new projects, creative opportunities and how we can help you.
+          </p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Get in touch</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/components/hero';
 import About from '@/components/about';
 import Services from '@/components/services';
+import Contact from '@/components/contact';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <Contact />
     </main>
   );
 }
