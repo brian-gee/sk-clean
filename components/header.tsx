bdf262dbdd1433705c6a6dddf42fae6dd4e416be
Brian Geertsma <briange@tuta.io>
2023-02-09 14:33:29 -0500

    updating header

--- components/header.tsx
+++ components/header.tsx
@@ -5,10 +5,7 @@ export default function Header() {
     <Head>
       <link rel="shortcut icon" href="/swimming-pool.png" />
       <title>SK Clean</title>
-      <meta
-        name="description"
-        content="The best clean for your pool."
-      />
+      <meta name="description" content="The best clean for your pool." />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
   );
