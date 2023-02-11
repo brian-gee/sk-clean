0ca5aa77fe468ce7b4d0e119f62e1bafdcf2d78e
Brian Geertsma <briange@tuta.io>
2023-02-11 04:14:19 -0500

    updating tracking and margin for hero text

--- components/hero.tsx
+++ components/hero.tsx
@@ -19,8 +19,8 @@ export default function Hero() {
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-center text-neutral-content">
             <div className="max-w-md">
-              <h1 className="mb-5 text-5xl">Your Pool</h1>
-              <h3 className="mb-5 text-3xl">Reimagined</h3>
+              <h1 className="mb-2 text-5xl tracking-wider">Your Pool</h1>
+              <h3 className="mb-5 text-3xl tracking-wider">Reimagined</h3>
               <button className="btn btn-primary">Get Started</button>
             </div>
           </div>
